01f6e1bf8ea7fb9c36c06d7f5ad88faae4152ff7
Tom Van Hoof <tomvanhoof4@msn.com>
2012-11-15 14:24:16 +0100

    added class schaap for test

---
+++ Schaap.java
@@ -0,0 +1,14 @@
+class Schaap{
+	public static void main(String[] args) {
+	Schaap awake = new Schaap();
+	Schaap asleep = new Schaap();
+	awake.meh();
+	asleep.zzz();
+	}
+	public void meh() {
+		System.out.println("beeee");
+	}
+	public void zzz() {
+		System.out.println("zzzzz");
+	}
+}
